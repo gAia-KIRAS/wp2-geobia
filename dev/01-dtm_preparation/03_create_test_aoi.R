fa004cd4737d7dbf40977498d4590c969665b5d5
Matthias Schlögl <matthias.schloegl@zamg.ac.at>
2022-02-21 14:47:18 +0100

    DEV: crop dtm to small test area

--- dev/01-dtm_preparation/03_create_test_aoi.R
+++ dev/01-dtm_preparation/03_create_test_aoi.R
@@ -1,6 +1,7 @@
 # load packages
 library("dplyr")
 library("sf")
+library("stars")
 
 # create polygon somewhere in Oberkärnten
 test_aoi <- c(
@@ -13,4 +14,16 @@ test_aoi <- c(
   st_sfc(crs = 31258)
 
 # export result
-st_write(extents, "dat/interim/aoi/test_area.gpkg")
+st_write(test_aoi, "dat/interim/aoi/test_area.gpkg")
+
+# full dtm
+dtm <- read_stars("dat/raw/dtm/dtm_ktn/ALS_DGM_1m.img")
+st_crs(dtm) <- 31258
+
+# crop to small aoi for testing purposes
+dtm_aoi <- dtm %>%
+  st_crop(test_aoi)
+
+write_stars(dtm_aoi, "dat/interim/dtm/test_aoi_ktn.tif")
+
+# gdalwarp -dstnodata -9999 -cutline dat/interim/aoi/test_area.gpkg dat/raw/dtm/dtm_ktn/ALS_DGM_1m.img dat/interim/dtm/test_aoi_ktn.tif
