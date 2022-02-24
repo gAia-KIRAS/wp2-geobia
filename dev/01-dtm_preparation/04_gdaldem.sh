8467d1205043936be001a0fd0ec09f7293b841ea
Matthias Schlögl <matthias.schloegl@zamg.ac.at>
2022-02-24 10:33:00 +0100

    DEV: improve logging

--- dev/01-dtm_preparation/04_gdaldem.sh
+++ dev/01-dtm_preparation/04_gdaldem.sh
@@ -2,22 +2,27 @@
 
 target_file="test_aoi_ktn"
 
-# compute slope
+# slope
+echo "\n`date "+%Y-%m-%d %H:%M:%S"`: working on '${target_file}'"
+
+# slope
 echo "\n`date "+%Y-%m-%d %H:%M:%S"`: compute slope"
 gdaldem slope -compute_edges dat/interim/dtm/${target_file}.tif dat/interim/dtm_derivates/${target_file}_slope.tif
 
-# compute aspect
+# aspect
 echo "\n`date "+%Y-%m-%d %H:%M:%S"`: compute aspect"
 gdaldem aspect -compute_edges dat/interim/dtm/${target_file}.tif dat/interim/dtm_derivates/${target_file}_aspect.tif
 
-# compute TRI
+# TRI
 echo "\n`date "+%Y-%m-%d %H:%M:%S"`: compute TRI"
 gdaldem TRI -compute_edges dat/interim/dtm/${target_file}.tif dat/interim/dtm_derivates/${target_file}_tri.tif
 
-# compute TPI
+# TPI
 echo "\n`date "+%Y-%m-%d %H:%M:%S"`: compute TPI"
 gdaldem TPI -compute_edges dat/interim/dtm/${target_file}.tif dat/interim/dtm_derivates/${target_file}_tpi.tif
 
-# compute roughness
+# roughness
 echo "\n`date "+%Y-%m-%d %H:%M:%S"`: compute roughness"
 gdaldem roughness -compute_edges dat/interim/dtm/${target_file}.tif dat/interim/dtm_derivates/${target_file}_roughness.tif
+
+echo "\n`date "+%Y-%m-%d %H:%M:%S"`: done"
