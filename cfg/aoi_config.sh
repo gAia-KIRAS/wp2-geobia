d6b709532e0b404422b6cfc68247b18c015a6670
Matthias Schlögl <matthias.schloegl@zamg.ac.at>
2022-04-14 13:08:40 +0000

    DEV: add target files for carinthia

--- cfg/aoi_config.sh
+++ cfg/aoi_config.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/zsh
 
-target_file="test_aoi_ktn"
+# File basename                              # file size
+# ------------------------------------------ #
+# target_file="dtm_carinthia_Nockberge_Ost"  #  7.2 G
+# target_file="dtm_carinthia_Nockberge_West" #  8.7 G
+# target_file="dtm_carinthia_Oberkaernten"   # 13.0 G
+# target_file="dtm_carinthia_Saualm_Koralm"  #  9.7 G
+# ------------------------------------------ #
+
+# File ending
 target_ending=".tif"
 
 # GRASS GIS location (subdirectory)
