cfd4b217b9640944bf62982d1a9ddf9a8c8cdc4d
Matthias Schlögl <matthias.schloegl@zamg.ac.at>
2022-07-19 16:08:10 +0200

    DEV: plot correlation heatmap

--- dev/01-dtm_preparation-vhr/08_assess_correlation.R
+++ dev/01-dtm_preparation-vhr/08_assess_correlation.R
@@ -2,6 +2,7 @@ library(stars)
 library(parallel)
 library(dplyr)
 library(tidyr)
+library(corrr)
 
 fl <- list.files(
   "dat/interim/dtm_derivates/ktn_Nockberge_Ost",
@@ -20,7 +21,24 @@ dem_to_mat <- function(x) {
 tifs <- mclapply(fl, dem_to_mat, mc.cores = 12L)
 names(tifs) <- param
 
-dat <- tifs %>%
+cor_dat <- tifs %>%
   bind_rows() %>%
   select(-`channel-network`, -`watershed-basins`) %>%
-  drop_na()
+  drop_na() %>%
+  cor() %>%
+  as.data.frame() %>%
+  rownames_to_column(var = "param_1") %>%
+  pivot_longer(-param_1, names_to = "param_2", values_to = "correlation") %>%
+  mutate(cor = round(correlation, 2))
+
+p <- ggplot(cor_dat, aes(x = param_1, y = param_2, fill = correlation)) +
+  geom_tile(show.legend = FALSE) +
+  geom_text(aes(label = cor), size = 2) +
+  coord_equal() +
+  scale_fill_gradient2(limits = c(-1, 1)) +
+  xlab("") +
+  ylab("") +
+  theme_linedraw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+ggsave("plt/dtm_correlation_heatmap.png", height = 250, width = 250, units = "mm", dpi = 300)
