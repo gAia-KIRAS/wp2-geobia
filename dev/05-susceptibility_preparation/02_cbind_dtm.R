1ab426a196aecab397d53c69a2edb363b4fd6cd9
Matthias Schlögl <matthias.schloegl@geosphere.at>
2023-06-02 15:08:29 +0200

    DEV: create dtm dataset

---
+++ dev/05-susceptibility_preparation/02_cbind_dtm.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(sf)
+library(glue)
+library(qs)
+library(tictoc)
+
+fl <- list.files("dat/interim/dtm_aoi/tmp_qs", pattern = "*.qs", full.names = TRUE)
+
+tic()
+res <- qread(fl[1], nthreads = 16L) |>
+  select(`aspect-arctan2`)
+toc()
+n <- nrow(res)
+
+for (f in fl[2:length(fl)]) {
+  print(glue("{Sys.time()} » Working on {basename(f)}"))
+  tmp <- f |>
+    qread(nthreads = 16L) |>
+    st_drop_geometry()
+  if (nrow(tmp) == n) {
+    res <- res |>
+      bind_cols(tmp)
+  } else {
+    warning(glue(">> Problem in {basename(f)}: nrow = {nrow(tmp)} <<"))
+  }
+}
+
+rm(f, tmp)
+gc()
+colnames(res)[9] <- "elevation"
+qsave(res, "dat/interim/dtm_aoi/dtm_full.qs", nthreads = 16L)
