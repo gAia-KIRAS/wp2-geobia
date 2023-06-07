7e67b15b301ba5f78b0716dc8343f6fda066ae68
Matthias Schlögl <matthias.schloegl@geosphere.at>
2023-06-07 16:27:19 +0200

    DEV: save convex hull instead of points

--- dev/05-susceptibility_preparation/00_define_grid.R
+++ dev/05-susceptibility_preparation/00_define_grid.R
@@ -14,4 +14,6 @@ colnames(grd)[1] <- "idx"
 grd$idx <- rep(1L, nrow(grd))
 
 qsave(grd, "dat/interim/aoi/gaia_ktn_grid.qs", nthreads = 16L)
-write_sf(grd, "dat/interim/aoi/gaia_ktn_grid.gpkg")
+
+ch <- st_convex_hull(st_union(grd)) 
+write_sf(ch, "dat/interim/aoi/gaia_ktn_grid.gpkg")
