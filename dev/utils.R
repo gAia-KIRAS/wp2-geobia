4bd7b2f9e3b09a01e2e2557c0602ecc9c157be15
Matthias Schlögl <matthias.schloegl@geosphere.at>
2023-06-07 17:47:22 +0200

    DEV: convert sfc to cols

--- dev/utils.R
+++ dev/utils.R
@@ -8,3 +8,18 @@ include <- function(str_list, regex_match) {
 exclude <- function(str_list, regex_match) {
   return(setdiff(str_list, include(str_list, regex_match)))
 }
+
+sfc_as_cols <- function(x, geometry, names = c("x", "y")) {
+  if (missing(geometry)) {
+    geometry <- sf::st_geometry(x)
+  } else {
+    geometry <- rlang::eval_tidy(enquo(geometry), x)
+  }
+  stopifnot(inherits(x, "sf") && inherits(geometry, "sfc_POINT"))
+  ret <- sf::st_coordinates(geometry)
+  ret <- tibble::as_tibble(ret)
+  stopifnot(length(names) == ncol(ret))
+  x <- x[, !names(x) %in% names]
+  ret <- setNames(ret, names)
+  dplyr::bind_cols(x, ret)
+}
