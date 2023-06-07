bdbd44e646ea17eaba1fa6e0f678d75c54a0595e
Matthias Schlögl <matthias.schloegl@geosphere.at>
2023-06-07 16:05:05 +0200

    DEV: create dataframe for target grid

---
+++ dev/05-susceptibility_preparation/00_define_grid.R
@@ -0,0 +1,17 @@
+library(dplyr)
+library(sf)
+library(stars)
+library(qs)
+
+aoi <- read_sf("dat/raw/aoi/gaia_projektgebiet_ktn.gpkg") |>
+  st_transform(3416)
+
+grd <- read_stars("dat/interim/dtm_derivates/austria/dtm_orig/dtm_austria.tif") |>
+  st_set_crs(3416) |>
+  st_crop(aoi) |>
+  st_as_sf(as_points = TRUE)
+colnames(grd)[1] <- "idx"
+grd$idx <- rep(1L, nrow(grd))
+
+qsave(grd, "dat/interim/aoi/gaia_ktn_grid.qs", nthreads = 16L)
+write_sf(grd, "dat/interim/aoi/gaia_ktn_grid.gpkg")
