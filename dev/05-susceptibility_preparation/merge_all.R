50770e2d0fc4ad13201a6c82f2ecf3e6e1432f8b
Matthias Schlögl <matthias.schloegl@geosphere.at>
2023-07-06 09:53:21 +0200

    DEV: add climate index data

--- dev/05-susceptibility_preparation/merge_all.R
+++ dev/05-susceptibility_preparation/merge_all.R
@@ -10,8 +10,12 @@ dtm <- qread("dat/interim/dtm_aoi/dtm_full.qs", nthreads = 64L)
 lc <- qread("dat/interim/misc_aoi/land_cover_full.qs", nthreads = 64L)
 identical(st_coordinates(dtm), st_coordinates(lc))
 
+ci <- qread("dat/interim/misc_aoi/climate_indices.qs", nthreads = 64L)
+identical(st_coordinates(dtm), st_coordinates(ci))
+
 out <- dtm |>
   bind_cols(lc |> st_drop_geometry()) |>
+  bind_cols(ci |> st_drop_geometry()) |>
   rename_with(.fn = \(x) gsub("-", "_", x), .cols = everything())
 
 st_write_parquet(obj = out, dsn = "dat/processed/carinthia_10m.parquet")
