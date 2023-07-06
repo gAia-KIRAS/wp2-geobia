1515d64bbbcc323e5fc8d01c644856a4c9bd1953
Matthias Schlögl <matthias.schloegl@geosphere.at>
2023-07-06 12:32:41 +0200

    DEV: set extent explicitly

--- dev/05-susceptibility_preparation/04a_surface_water_warp.sh
+++ dev/05-susceptibility_preparation/04a_surface_water_warp.sh
@@ -11,6 +11,7 @@ do
   echo "Working on $intiff:t"
   outtiff="./interim/oberflaechenabfluss/prep/$intiff:t"
   gdalwarp -tr 10 -10 -r med -tap -t_srs EPSG:3416 \
+    -te 348660 300970 532530 359210 \
     -cutline raw/aoi/gaia_projektgebiet_ktn.gpkg -cl gaia_projektgebiet -crop_to_cutline -of GTiff \
     $intiff $outtiff
 done
