ade2c33144a82920ab4effb858eea31650ee1738
Matthias Schlögl <matthias.schloegl@geosphere.at>
2023-07-06 12:12:13 +0200

    DEV: unzip, aggregate, crop

---
+++ dev/05-susceptibility_preparation/04a_surface_water_warp.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/zsh
+
+cd dat
+unzip raw/oberflaechenabfluss/KTN_Gefahrenkategorien.zip -d interim/oberflaechenabfluss/orig/
+7z e raw/oberflaechenabfluss/KTN_MaxWassertiefe.zip -ointerim/oberflaechenabfluss/orig/
+7z e raw/oberflaechenabfluss/KTN_MaxGeschwindigkeit.zip -ointerim/oberflaechenabfluss/orig/
+7z e raw/oberflaechenabfluss/KTN_SpezifischerAbfluss.zip -ointerim/oberflaechenabfluss/orig/
+
+for intiff in ./interim/oberflaechenabfluss/orig/*tif;
+do
+  echo "Working on $intiff:t"
+  outtiff="./interim/oberflaechenabfluss/prep/$intiff:t"
+  gdalwarp -tr 10 -10 -r med -tap -t_srs EPSG:3416 \
+    -cutline raw/aoi/gaia_projektgebiet_ktn.gpkg -cl gaia_projektgebiet -crop_to_cutline -of GTiff \
+    $intiff $outtiff
+done
