49706e1666b947af0fb82bba6dcc5a9ca94a4698
Matthias Schlögl <matthias.schloegl@geosphere.at>
2023-07-10 14:43:55 +0200

    DEV: increase number of cores

--- dev/05-susceptibility_preparation/00_define_grid.R
+++ dev/05-susceptibility_preparation/00_define_grid.R
@@ -6,6 +6,7 @@ library(qs)
 aoi <- read_sf("dat/raw/aoi/gaia_projektgebiet_ktn.gpkg") |>
   st_transform(3416)
 
+# nrow: 57,842,689
 grd <- read_stars("dat/interim/dtm_derivates/austria/dtm_orig/dtm_austria.tif") |>
   st_set_crs(3416) |>
   st_crop(aoi) |>
@@ -13,7 +14,7 @@ grd <- read_stars("dat/interim/dtm_derivates/austria/dtm_orig/dtm_austria.tif") 
 colnames(grd)[1] <- "idx"
 grd$idx <- rep(1L, nrow(grd))
 
-qsave(grd, "dat/interim/aoi/gaia_ktn_grid.qs", nthreads = 16L)
+qsave(grd, "dat/interim/aoi/gaia_ktn_grid.qs", nthreads = 64L)
 
 ch <- st_convex_hull(st_union(grd))
 write_sf(ch, "dat/interim/aoi/gaia_ktn_grid.gpkg")
