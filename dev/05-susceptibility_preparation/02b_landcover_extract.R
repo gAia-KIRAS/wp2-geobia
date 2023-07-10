b879077a68fd2ca520dcd93eeeb83108be62c3fc
Matthias Schlögl <matthias.schloegl@geosphere.at>
2023-07-10 12:52:55 +0200

    DEV: increase number of cores

--- dev/05-susceptibility_preparation/02b_landcover_extract.R
+++ dev/05-susceptibility_preparation/02b_landcover_extract.R
@@ -3,21 +3,24 @@ library(sf)
 library(stars)
 library(qs)
 
+ncores <- 64L
+
 # AOI
 aoi <- read_sf("dat/raw/aoi/gaia_projektgebiet_ktn.gpkg") |>
   st_transform(3416)
 
 # AOI dataframe
-grd <- qread("dat/interim/aoi/gaia_ktn_grid.qs", nthreads = 16L)
+grd <- qread("dat/interim/aoi/gaia_ktn_grid.qs", nthreads = ncores)
 
 # Forest cover
 wk <- read_stars("dat/interim/misc_aoi/wk_aoi_ktn.tif") |>
+  st_crop(aoi) |>
   st_as_sf(as_points = TRUE) |>
   rename(forest_cover = wk_aoi_ktn.tif)
 res_wk <- st_join(grd, wk) |>
   select(-idx)
 nrow(grd) == nrow(res_wk)
-qsave(res_wk, "dat/interim/misc_aoi/forest_cover.qs", nthreads = 16L)
+qsave(res_wk, "dat/interim/misc_aoi/forest_cover.qs", nthreads = ncores)
 
 # Corine Land Cover
 clc <- read_sf("dat/raw/clc/CLC18_AT_clip.shp") |>
@@ -27,10 +30,10 @@ clc <- read_sf("dat/raw/clc/CLC18_AT_clip.shp") |>
 res_clc <- st_join(grd, clc) |>
   select(-idx)
 nrow(grd) == nrow(res_clc)
-qsave(res_clc, "dat/interim/misc_aoi/clc.qs", nthreads = 16L)
+qsave(res_clc, "dat/interim/misc_aoi/clc.qs", nthreads = ncores)
 
 res <- res_clc |>
   # mutate(forest_cover = as.integer(res_wk$forest_cover)) |>
   mutate(clc = as.factor(CLC)) |>
   select(clc, forest_cover, geometry)
-qsave(res, "dat/interim/misc_aoi/land_cover_full.qs", nthreads = 16L)
+qsave(res, "dat/interim/misc_aoi/land_cover_full.qs", nthreads = ncores)
