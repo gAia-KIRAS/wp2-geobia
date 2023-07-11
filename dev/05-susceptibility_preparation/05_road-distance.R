5a755185ea4322ff82b136c312c1f9e3216ad391
Matthias Schlögl <matthias.schloegl@geosphere.at>
2023-07-11 12:46:15 +0200

    DEV: sample points along lines

--- dev/05-susceptibility_preparation/05_road-distance.R
+++ dev/05-susceptibility_preparation/05_road-distance.R
@@ -10,10 +10,19 @@ gip <- read_sf("dat/interim/gip/kaernten.gpkg") |>
   st_transform(3416) |>
   filter(FRC %in% c(0:11, 20, 21, 105, 106)) |>
   filter(BAUSTATUS == 5) |>
-  rename(gip_id = ACTION_ID) |>
-  select(gip_id, geom)
+  select(gip_id = ACTION_ID, length = SHAPELENGTH, geom)
+summary(gip$length)
+
+tic()
+gip_point <- gip |>
+  st_cast("LINESTRING") |>
+  st_line_sample(density = 0.2)
+toc()
+nrow(gip)
+length(gip_point)
 
 grd <- qread("dat/interim/aoi/gaia_ktn_grid.qs", nthreads = ncores)
+st_crs(gip_point) == st_crs(grd)
 
 # when using projected points, calculation is done using nabor::knn, a fast search method based on the libnabo C++ library
 # setting parallel = ncores is not applicable
