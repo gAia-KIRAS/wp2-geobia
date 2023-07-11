37d62a389a0550d7de8a29f0c7cdda63380dd2c5
Matthias Schlögl <matthias.schloegl@geosphere.at>
2023-07-11 11:26:44 +0200

    DEV: draft distance computation

---
+++ dev/05-susceptibility_preparation/05_road-distance.R
@@ -0,0 +1,19 @@
+library(dplyr)
+library(sf)
+library(nngeo)
+library(qs)
+library(tictoc)
+
+ncores <- 64L
+
+gip <- read_sf("dat/interim/gip/kaernten.gpkg") |>
+  st_transform(3416)
+grd <- qread("dat/interim/aoi/gaia_ktn_grid.qs", nthreads = ncores)
+
+# when using projected points, calculation is done using nabor::knn, a fast search method based on the libnabo C++ library
+# setting parallel = ncores is not applicable
+tic()
+res <- st_nn(grd, gip, sparse = TRUE, k = 1, maxdist = 1000, progress = TRUE)
+toc()
+
+qsave(res, "dat/interim/misc_aoi/road_dist.qs", nthreads = ncores)
