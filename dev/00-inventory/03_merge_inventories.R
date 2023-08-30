54b684509fa715623a8b7478b633ced2d4912da8
Matthias Schlögl <matthias.schloegl@geosphere.at>
2023-08-30 10:58:37 +0200

    DEV: merge GEORIOS to ALS and KAGIS data
    Refs #14

---
+++ dev/00-inventory/03_merge_inventories.R
@@ -0,0 +1,20 @@
+library(sf)
+library(dplyr)
+
+# GEORIOS export (2023-08-01)
+georios <- read_sf("dat/processed/inventory_date_info.gpkg") |>
+  mutate(source = "GEORIOS") |>
+  select(gr_nr = GR_NR, process = PROCESS, source)
+
+ls_scars_merge <- read_sf("dat/interim/inventory/LS_scars_merge.gpkg") |>
+  st_transform(3416) |>
+  mutate(id = row_number()) |>
+  select(id, source = Grundlage) |>
+  mutate(source = if_else(is.na(source), "KAGIS", source)) |>
+  mutate(source = gsub(" Ereigniskataster", "", source))
+
+inventory <- georios |>
+  bind_rows(ls_scars_merge) |>
+  select(source, gr_nr, id, process, geom)
+
+st_write(inventory, "dat/interim/inventory/LS_scars_merge_georios.gpkg")
