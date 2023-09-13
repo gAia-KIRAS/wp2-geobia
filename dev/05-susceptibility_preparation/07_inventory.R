6167948657ade9382494bb5b8c5ae1c58f550cf6
Matthias Schlögl <matthias.schloegl@geosphere.at>
2023-09-13 14:48:54 +0200

    DEV: use only validated inventory

--- dev/05-susceptibility_preparation/07_inventory.R
+++ dev/05-susceptibility_preparation/07_inventory.R
@@ -11,28 +11,33 @@ source("dev/utils.R")
 
 ncores <- 32L
 
-process_types <- tribble(
-  ~ID, ~PROCESS,
-  "130.500", "Bereich mit Rutsch- / Gleitprozessen",
-  "130.000", "Gleiteh/Rutschen",
-  "144.000", "Hangmure",
-  "132.000", "Lockergesteinsrutschung",
-  "152.000", "LG-Rutschung bis Hangmure",
-  "192.000", "Massenbewegung im LG",
-  "194.000", "Rutschung oder Hangmure",
-  "195.000", "Uferanbruch bzw. Rutschung"
-)
+# process_types <- tribble(
+#   ~ID, ~PROCESS,
+#   "130.500", "Bereich mit Rutsch- / Gleitprozessen",
+#   "130.000", "Gleiteh/Rutschen",
+#   "144.000", "Hangmure",
+#   "132.000", "Lockergesteinsrutschung",
+#   "152.000", "LG-Rutschung bis Hangmure",
+#   "192.000", "Massenbewegung im LG",
+#   "194.000", "Rutschung oder Hangmure",
+#   "195.000", "Uferanbruch bzw. Rutschung"
+# )
 
 wall("{Sys.time()} -- reading inventories")
 
+# AOI
+# aoi <- read_sf("dat/raw/aoi/gaia_projektgebiet_ktn.gpkg") |>
+#   st_transform(3416)
+
 # after filting by process type, 1415/1962 events remain
-inv_georios <- read_sf("dat/interim/inventory/GEORIOS_for_gAia.gpkg") |>
-  filter(CODE %in% process_types$ID) |>
-  st_geometry() |>
-  st_transform(3416) |>
-  st_as_sf() |>
-  rename(geom = x) |>
-  mutate(slide = 1L)
+# inv_georios <- read_sf("dat/interim/inventory/GEORIOS_for_gAia.gpkg") |>
+#   filter(CODE %in% process_types$ID) |>
+#   st_geometry() |>
+#   st_transform(3416) |>
+#   st_as_sf() |>
+#   rename(geom = x) |>
+#   mutate(slide = 2L) |>
+#   st_intersection(aoi)
 
 # 2704 events
 inv_valid <- read_sf("dat/interim/inventory/LS_scars_merge.gpkg") |>
@@ -40,22 +45,24 @@ inv_valid <- read_sf("dat/interim/inventory/LS_scars_merge.gpkg") |>
   st_transform(3416) |>
   st_as_sf() |>
   rename(geom = x) |>
-  mutate(slide = 2L)
+  mutate(slide = 1L)
 
 # combine inventory locations: 4119 events
-inv <- bind_rows(inv_valid, inv_georios) |>
-  st_buffer(dist = units::as_units(5, "m"))
-wall("    number of landslide events (validated: 2, not validated: 1)")
-table(inv$slide)
+# inv <- bind_rows(inv_valid, inv_greorios) |>
+# st_buffer(dist = units::as_units(5, "m")) |>
+#  group_by(slide) |>
+#  summarize(geom = st_union(geom))
+
+# buffer only validated inventory
+inv <- inv_valid |>
+  st_buffer(dist = units::as_units(5, "m")) |>
+  group_by(slide) |>
+  summarize(geom = st_union(geom))
 
 wall("{Sys.time()} -- reading target grid")
 grd <- qread("dat/interim/aoi/gaia_ktn_grid.qs", nthreads = ncores)
 
-wall("{Sys.time()} -- performing spatial join")
-# left spatial join results in 184 duplicates
-# 0: 57840030 --> 57840030
-# 1:      706 -->      675
-# 2:     2137 -->     1984
+wall("{Sys.time()} -- performing spatial join") # 500 sec elapsed
 tic()
 inventory <- st_join(grd, inv, join = st_intersects, left = TRUE) |>
   select(-idx) |>
@@ -63,10 +70,11 @@ inventory <- st_join(grd, inv, join = st_intersects, left = TRUE) |>
   st_drop_geometry() |>
   mutate(across(x:y, as.integer)) |>
   mutate(slide = tidyr::replace_na(slide, 0)) |>
-  group_by(x, y) |>
-  summarise(slide = max(slide)) |>
-  ungroup()
+  mutate(slide = as.logical(slide))
 toc()
+
+# 0: 57840705
+# 1:     1984
 
 stopifnot(nrow(inventory) == nrow(grd))
 
