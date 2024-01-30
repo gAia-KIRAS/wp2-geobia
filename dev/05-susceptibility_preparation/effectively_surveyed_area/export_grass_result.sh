32e064d7b3a0b876474ce83d993903b9286f8afb
Matthias Schlögl <matthias.schloegl@geosphere.at>
2024-01-30 09:15:36 +0100

    DEV: export geotiff from grass raster

---
+++ dev/05-susceptibility_preparation/effectively_surveyed_area/export_grass_result.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/zsh
+
+grassdir="dat/grassdata/grass_db/carinthia/esa"
+outdir="dat/interim/effectively_surveyed_area"
+
+raster="res_distance_rescaled"
+#raster="res_numberofviews"
+#raster="res_pointofviews"
+#raster="res_viewangles"
+
+grass ${grassdir} --exec r.out.gdal input=res_distance_rescaled output="${outdir}/${raster}.tif" format="GTiff" nodata=0
+
