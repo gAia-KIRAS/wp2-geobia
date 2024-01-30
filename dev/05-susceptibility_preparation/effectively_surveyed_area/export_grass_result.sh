cc4c483ab95d86ee34d81eb648e4e3e719fc02ec
Matthias Schlögl <matthias.schloegl@geosphere.at>
2024-01-30 16:50:32 +0100

    DEV: set nan to zero

--- dev/05-susceptibility_preparation/effectively_surveyed_area/export_grass_result.sh
+++ dev/05-susceptibility_preparation/effectively_surveyed_area/export_grass_result.sh
@@ -8,6 +8,8 @@ raster="res_distance_rescaled"
 #raster="res_pointofviews"
 #raster="res_viewangles"
 
-grass ${grassdir} --exec r.out.gdal input=res_distance_rescaled output="${outdir}/${raster}.tif" format="GTiff" nodata=0
+grass ${grassdir} --exec r.out.gdal input=res_distance_rescaled output="${outdir}/${raster}.tif" format="GTiff" nodata=-1
 
-gdal_calc.py -A ${outdir}/${raster}.tif --outfile=${outdir}/${raster}_binary.tif --calc="A>0" --type "Byte"
+gdal_edit.py ${outdir}/${raster}.tif -unsetnodata
+# gdal_calc.py -A ${outdir}/${raster}.tif --outfile="${outdir}/${raster}.tif" --calc="nan_to_num(A)" --overwrite
+gdal_calc.py -A ${outdir}/${raster}.tif --outfile="${outdir}/${raster}_binary.tif" --calc="A>0" --type "Byte" --overwrite
