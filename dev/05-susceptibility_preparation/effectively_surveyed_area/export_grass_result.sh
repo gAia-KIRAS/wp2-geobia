4349aa60932c96e2a7c169c5811e57c26fb2082d
Matthias Schlögl <matthias.schloegl@geosphere.at>
2024-01-30 09:17:55 +0100

    DEV: convert output to boolean

--- dev/05-susceptibility_preparation/effectively_surveyed_area/export_grass_result.sh
+++ dev/05-susceptibility_preparation/effectively_surveyed_area/export_grass_result.sh
@@ -10,3 +10,4 @@ raster="res_distance_rescaled"
 
 grass ${grassdir} --exec r.out.gdal input=res_distance_rescaled output="${outdir}/${raster}.tif" format="GTiff" nodata=0
 
+gdal_calc.py -A ${outdir}/${raster}.tif --outfile=${outdir}/${raster}_binary.tif --calc="A>0" --type "Byte"
