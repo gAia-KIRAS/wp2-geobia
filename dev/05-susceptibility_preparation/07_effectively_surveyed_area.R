9403b8aadf9ab0dafcaf57d4667016660f1765f7
Matthias Schlögl <matthias.schloegl@geosphere.at>
2024-01-31 12:34:19 +0100

    DEV: use cropped dataset from d6ee6a89

--- dev/05-susceptibility_preparation/07_effectively_surveyed_area.R
+++ dev/05-susceptibility_preparation/07_effectively_surveyed_area.R
@@ -9,11 +9,12 @@ source("dev/utils.R")
 aoi <- read_sf("dat/raw/aoi/gaia_projektgebiet_ktn.gpkg") |>
   st_transform(3416)
 
-esa <- read_stars("dat/interim/effectively_surveyed_area/res_distance_rescaled_binary.tif") |>
+esa <- read_stars("dat/interim/effectively_surveyed_area/res_distance_rescaled_binary_clipped.tif") |>
   #st_crop(aoi) |>
-  st_as_sf(as_points = TRUE)
+  st_as_sf(as_points = TRUE) |>
+  rename(esa = res_distance_rescaled_binary_clipped.tif) |>
+  mutate(esa = as.integer(esa))
+
 st_crs(esa) # 3416
-colnames(esa)[1] <- "esa"
-esa <- esa |> mutate(esa = as.integer(esa))
 
 qsave(esa, "dat/interim/misc_aoi/esa.qs", nthreads = 16L)
