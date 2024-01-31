d9d6a6c93c822ddaabdefddc9903d0e4f6f589a2
Matthias Schlögl <matthias.schloegl@geosphere.at>
2024-01-31 09:43:31 +0100

    BUG: mutate after defining colname

--- dev/05-susceptibility_preparation/07_effectively_surveyed_area.R
+++ dev/05-susceptibility_preparation/07_effectively_surveyed_area.R
@@ -10,10 +10,10 @@ aoi <- read_sf("dat/raw/aoi/gaia_projektgebiet_ktn.gpkg") |>
   st_transform(3416)
 
 esa <- read_stars("dat/interim/effectively_surveyed_area/res_distance_rescaled_binary.tif") |>
-  st_crop(aoi) |>
-  st_as_sf(as_points = TRUE) |>
-  mutate(esa = as.integer(esa))
+  #st_crop(aoi) |>
+  st_as_sf(as_points = TRUE)
 st_crs(esa) # 3416
 colnames(esa)[1] <- "esa"
+esa <- esa |> mutate(esa = as.integer(esa))
 
 qsave(esa, "dat/interim/misc_aoi/esa.qs", nthreads = 16L)
