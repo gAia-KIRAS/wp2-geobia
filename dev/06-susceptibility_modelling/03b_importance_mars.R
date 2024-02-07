9a028eae5b8567085e1ebeed61266b6adf69b357
Matthias Schlögl <matthias.schloegl@geosphere.at>
2024-02-07 08:57:29 +0000

    DEV: prettify plot

--- dev/06-susceptibility_modelling/03b_importance_mars.R
+++ dev/06-susceptibility_modelling/03b_importance_mars.R
@@ -12,14 +12,16 @@ ids <- glue("iteration-{sprintf('%02d', 1:10)}")
 
 wall("{Sys.time()} -- reading data")
 lut_names <- read_csv("doc/data_description/lut_vars.csv")
-rf_mods <- readRDS("dat/interim/mars/earth_mbo.rds")
-imp_lst <- lapply(rf_mods, get_evimp)
+earth_mods <- readRDS("dat/interim/mars/earth_mbo.rds")
+imp_lst <- lapply(earth_mods, get_evimp)
 
 # aggregated importance from models ----
 imp <- imp_lst |>
   bind_rows(.id = "id") |>
   mutate(id = gsub("iteration-", "", id)) |>
   pivot_longer(cols = nsubsets:rss, names_to = "metric") |>
+  mutate(index = gsub("land_cover93", "land_cover", index)) |>
+  mutate(index = gsub("lithologySchwemmkegel und Schwemmfaecher", "lithology", index)) |>
   group_by(index, metric) |>
   summarize(
     min_imp = min(value),
@@ -36,11 +38,11 @@ p <- ggplot(imp, aes(x = feature_name, y = mean_imp, color = progenitor)) +
   geom_pointrange(aes(ymin = min_imp, ymax = max_imp)) +
   coord_flip() +
   xlab("feature") +
-  ylab("permutation feature importance") +
+  ylab("importance") +
   guides(color = guide_legend(title = "type")) +
   scale_color_discrete_qualitative("Dark3") +
   scale_alpha(range = c(0.4, 1), guide = "none") +
-  facet_wrap(~metric) +
+  facet_wrap(~metric, scales = "free_x") +
   theme_linedraw() +
   theme(
     text = element_text(
@@ -51,4 +53,4 @@ p <- ggplot(imp, aes(x = feature_name, y = mean_imp, color = progenitor)) +
     legend.position = "right"
   )
 
-ggsave("plt/importance.png", p, width = 180, height = 120, units = "mm")
+ggsave("plt/importance_mars.png", p, width = 180, height = 120, units = "mm")
