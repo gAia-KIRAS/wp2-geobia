e1f5f059474e907f849bc7876736bcf7c17af038
Matthias Schlögl <matthias.schloegl@geosphere.at>
2024-02-08 16:21:11 +0000

    REF: use the power of linear algebra

--- dev/05-susceptibility_preparation/11a_compute_correlations.R
+++ dev/05-susceptibility_preparation/11a_compute_correlations.R
@@ -5,20 +5,27 @@ suppressPackageStartupMessages({
   library("dplyr")
   library("tidyr")
   library("purrr")
-  library("ggplot2")
 })
 
 print(glue::glue("{Sys.time()} -- reading data"))
 res <- read_ipc_file("dat/processed/carinthia_10m.arrow") |>
   select(-flow_path_length, -flow_width, -sca, -esa, -x, -y) |>
-  select(where(is.numeric))
+  select(where(is.numeric)) |>
+  slice_sample(prop = 0.5) |>
+  as.matrix()
+
+# compute corr
+fastcor <- function(x) {
+  1 / (NROW(x) - 1) * crossprod(scale(x, TRUE, TRUE))
+}
+
 
 print(glue::glue("{Sys.time()} -- computing correlations"))
 compute_corr <- function(cn1, cn2, dat = res) {
   if (cn1 == cn2) {
     out <- 1
   } else {
-    out <- cor(dat[cn1], dat[cn2])
+    out <- cor(dat[, cn1], dat[, cn2])
   }
   return(out)
 }
