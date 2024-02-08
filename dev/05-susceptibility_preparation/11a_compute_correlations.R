968d18992048d1f4d8151575c0a83cca51822a44
Matthias Schlögl <matthias.schloegl@geosphere.at>
2024-02-08 14:25:28 +0000

    DEV: compute pairwise correlations

---
+++ dev/05-susceptibility_preparation/11a_compute_correlations.R
@@ -0,0 +1,27 @@
+library("arrow")
+library("dplyr")
+library("tidyr")
+library("purrr")
+library("ggplot2")
+
+res <- read_ipc_file("dat/processed/carinthia_10m.arrow") |>
+  select(-flow_path_length, -flow_width, -sca, -esa, -x, -y) |>
+  select(where(is.numeric))
+
+compute_corr <- function(cn1, cn2, dat = res) {
+  if (cn1 == cn2) {
+    out <- 1
+  } else {
+    out <- cor(dat[cn1], dat[cn2])
+  }
+  return(out)
+}
+
+cn <- colnames(res)
+corrs <- crossing(cn, cn)
+corr <- map2_dbl(corrs$cn...1, corrs$cn...2, compute_corr, .progress = TRUE)
+corrs$corr <- corr
+rm(res, cn, corr)
+gc()
+
+saveRDS(corrs, "dat/interim/correlations.rds")
