d42c1ad59edbbf0b10469b1b5ad555e4ddfebb3b
Matthias Schlögl <matthias.schloegl@geosphere.at>
2024-02-08 14:29:14 +0000

    DOC: add logging

--- dev/05-susceptibility_preparation/11a_compute_correlations.R
+++ dev/05-susceptibility_preparation/11a_compute_correlations.R
@@ -1,13 +1,19 @@
-library("arrow")
-library("dplyr")
-library("tidyr")
-library("purrr")
-library("ggplot2")
+print(glue::glue("{Sys.time()} -- loading packages"))
 
+suppressPackageStartupMessages({
+  library("arrow")
+  library("dplyr")
+  library("tidyr")
+  library("purrr")
+  library("ggplot2")
+})
+
+print(glue::glue("{Sys.time()} -- reading data"))
 res <- read_ipc_file("dat/processed/carinthia_10m.arrow") |>
   select(-flow_path_length, -flow_width, -sca, -esa, -x, -y) |>
   select(where(is.numeric))
 
+print(glue::glue("{Sys.time()} -- computing correlations"))
 compute_corr <- function(cn1, cn2, dat = res) {
   if (cn1 == cn2) {
     out <- 1
@@ -25,3 +31,5 @@ rm(res, cn, corr)
 gc()
 
 saveRDS(corrs, "dat/interim/correlations.rds")
+
+print(glue::glue("{Sys.time()} -- done"))
