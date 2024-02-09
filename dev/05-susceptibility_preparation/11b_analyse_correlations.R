5d4c52ba2c92616492e703ffa2c531cd56e097d4
Matthias Schlögl <matthias.schloegl@geosphere.at>
2024-02-09 08:03:08 +0000

    DEV: plot correlations

---
+++ dev/05-susceptibility_preparation/11b_analyse_correlations.R
@@ -0,0 +1,34 @@
+print(glue::glue("{Sys.time()} -- loading packages"))
+
+suppressPackageStartupMessages({
+  library("tidyverse")
+  library("glue")
+  library("qs")
+  library("colorspace")
+  library("showtext")
+})
+
+font_add("Source Sans Pro", "~/.fonts/source-sans-pro/SourceSansPro-Regular.ttf")
+showtext_auto()
+
+corrs <- read_rds("dat/interim/correlations.rds") |>
+  rename(correlation = corr)
+
+p <- ggplot(corrs, aes(x = cn...1, y = cn...2)) +
+  geom_raster(aes(fill = correlation)) +
+  xlab("") +
+  ylab("") +
+  scale_fill_continuous_diverging("Vik", limits = c(-1, 1)) +
+  theme_linedraw() +
+  theme(
+    text = element_text(
+      family = "Source Sans Pro",
+      colour = "black",
+      size = 20
+    ),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+    legend.position = "right"
+  )
+
+ggsave("plt/correlation.png", p, width = 130, height = 120, units = "mm")
+print(glue::glue("{Sys.time()} -- done"))
