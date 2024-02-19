4cc9b0fbd9d1a0bf24df79d118d1434ee83dfb23
Matthias Schlögl <matthias.schloegl@geosphere.at>
2024-02-19 14:28:10 +0000

    ENH: add support for plotting factors

--- dev/06-susceptibility_modelling/07b_pdp+ice_plot.R
+++ dev/06-susceptibility_modelling/07b_pdp+ice_plot.R
@@ -22,20 +22,52 @@ dat <- qread("dat/interim/random_forest/pdp+ice.qs", nthreads = ncores)
 features <- names(dat)
 lut_names <- read_csv("doc/data_description/lut_vars.csv")
 
+factors <- c("land_cover", "forest_cover", "geomorphons", "lithology")
+
+lut_lc <- tribble(
+    ~land_cover, ~lc,
+    11, "Built-up",
+    12, "Flat sealed surfaces",
+    31, "Permanent soil",
+    32, "Bare rock and screes",
+    60, "Water",
+    70, "Snow and ice",
+    91, "Trees - broad leaved",
+    93, "Trees - coniferous",
+    100, "Bushes and shrubs",
+    122, "Herbaceous periodically",
+    125, "Herbaceous permanent -\nlow productivity",
+    126, "Herbaceous permanent -\nhigh productivity",
+    130, "Reeds"
+)
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 export_plot <- function(feature, data = dat) {
   res <- data[[feature]] |>
     filter(iteration == 1)
+  if(feature == "land_cover") {
+    res <- res |>
+      mutate(land_cover = as.integer(as.character(land_cover))) |>
+      left_join(lut_lc, by = join_by(land_cover)) |>
+      select(iteration, land_cover = lc, .class, .value, .type)
+  }
   pdp <- res |>
     filter(.type == "pdp") |>
     select(iteration:.value)
   varname <- lut_names |>
     filter(feature_shortname == feature) |>
     pull(feature_name)
-  p <- ggplot(res, aes(x = .data[[feature]], y = .value)) +
-    geom_line(aes(group = .id), alpha = 0.1) +
-    geom_line(data = pdp, color = "#E69F00", linewidth = 1) +
+  if(feature %in% factors) {
+    p <- ggplot(res, aes(x = .data[[feature]], y = .value)) +
+      geom_boxplot() +
+      geom_point(data = pdp, color = "#E69F00", size = 2)
+  } else {
+    p <- ggplot(res, aes(x = .data[[feature]], y = .value)) +
+      geom_line(aes(group = .id), alpha = 0.1) +
+      geom_line(data = pdp, color = "#E69F00", linewidth = 1)
+  }
+  p <- p +
     facet_wrap(~.class) +
     theme_linedraw() +
     theme(
@@ -48,6 +80,9 @@ export_plot <- function(feature, data = dat) {
     ) +
     xlab(label = varname) +
     scale_y_continuous(name = "predicted landslide probatility", limits = c(0, 1))
+  if(feature %in% factors) {
+    p <- p + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+  }
   ggsave(glue("plt/pdp_ice_{feature}.png"), p, width = 240, height = 120, units = "mm")
 }
 
