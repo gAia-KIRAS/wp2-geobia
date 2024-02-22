63b978c936472b2efef78e8a465aa4b88f47c282
Matthias Schlögl <matthias.schloegl@geosphere.at>
2024-02-22 16:21:24 +0100

    DEV: update geology with reclass version

--- dev/05-susceptibility_preparation/06_geology.R
+++ dev/05-susceptibility_preparation/06_geology.R
@@ -2,7 +2,7 @@ library(sf)
 library(dplyr)
 library(qs)
 
-ncores <- 12L
+ncores <- 16L
 
 # AOI
 aoi <- read_sf("dat/raw/aoi/gaia_projektgebiet_ktn.gpkg") |>
@@ -11,14 +11,23 @@ aoi <- read_sf("dat/raw/aoi/gaia_projektgebiet_ktn.gpkg") |>
 # AOI dataframe
 grd <- qread("dat/interim/aoi/gaia_ktn_grid.qs", nthreads = ncores)
 
+litho_reclass <- readr::read_tsv("doc/data_description/reclass_geology.tsv") |>
+  mutate(
+    lithology = as.integer(class),
+    leg_litho = as.integer(leg_litho)
+  ) |>
+  select(leg_litho, lithology)
+
 # wget https://gis.geologie.ac.at/inspire/download/insp_ge_gu_500k_epsg4258.gpkg
 st_layers("dat/raw/geology/200k/Geologie_Kaernten_200.000.gpkg")
 lithology <- read_sf("dat/raw/geology/200k/Geologie_Kaernten_200.000.gpkg", layer = "Geologie 200.000") |>
-  select(lithology = lithologie) |>
-  mutate(lithology = as.factor(lithology)) |>
-  st_transform(3416)
+  st_transform(3416) |>
+  select(leg_litho, geom) |>
+  left_join(litho_reclass, by = "leg_litho") |>
+  select(lithology, geom)
 
 res_lithology <- st_join(grd, lithology) |>
-  select(-idx)
+  select(-idx) |>
+  mutate(lithology = as.factor(lithology))
 
 qsave(res_lithology, "dat/interim/misc_aoi/lithology_full.qs", nthreads = ncores)
