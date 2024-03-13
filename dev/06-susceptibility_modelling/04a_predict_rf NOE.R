94525955598f33585801119afc8f69b0de911734
anna <anna.kozlowska@ait.ac.at>
2024-03-13 09:24:53 +0100

    04 and 05 updates

--- dev/06-susceptibility_modelling/04a_predict_rf NOE.R
+++ dev/06-susceptibility_modelling/04a_predict_rf NOE.R
@@ -52,7 +52,7 @@ rf_mods <- readRDS("wp2-geobia/dat/interim/random_forest/ranger_mbo.rds")
 # print(glue::glue("{Sys.time()} -- working on negative chunks"))
 # neg_lst <- list.files("dat/processed/chunks/neg", recursive = TRUE, full.names = TRUE)
 
-# working on chunks #To DO next
+# working on chunks
 print(glue::glue("{Sys.time()} -- working on chunks"))
 chk_lst <- list.files("wp2-geobia/dat/processed/chunks/all", recursive = TRUE, full.names = TRUE)
 for (f in chk_lst) {
@@ -67,17 +67,19 @@ for (f in chk_lst) {
   }
 }
 
-# for NOE no chunks but full dataset
-print(glue::glue("{Sys.time()} -- reading data"))
-dat_iters <- read_dat("wp2-geobia/dat/processed/gaia_noe_balanced_iters.arrow") #wrong, this is balanced data set, i need to predict on the full dataset
+# # for NOE no chunks but full dataset #ignored as it needed to be chunked.
+# ## DIDN'T WORK, most likely because of RAM (needed to chunk the data instead).
 
-# full data set
-dat_iters_arrow <- arrow::read_feather("wp2-geobia/dat/processed/noe_10m.arrow")
+# print(glue::glue("{Sys.time()} -- reading data"))
+# dat_iters <- read_dat("wp2-geobia/dat/processed/gaia_noe_balanced_iters.arrow") #wrong, this is balanced data set, i need to predict on the full dataset
 
-# dat_iters <- arrow::read_parquet("wp2-geobia/dat/processed/noe_10m.parquet")
+# # full data set
+# dat_iters_arrow <- arrow::read_feather("wp2-geobia/dat/processed/noe_10m.arrow")
 
-dat_iters_arrow_cld <- drop_na(dat_iters_arrow)
-print(glue::glue("{Sys.time()} -- predicting"))
-predict_ensemble(rf_mods, dat_iters_arrow_cld) %>%
-  qsave("wp2-geobia/dat/processed/prediction/random_forest/random_forest_prediction.qs", nthreads = ncores)
+# # dat_iters <- arrow::read_parquet("wp2-geobia/dat/processed/noe_10m.parquet")
+
+# dat_iters_arrow_cld <- drop_na(dat_iters_arrow)
+# print(glue::glue("{Sys.time()} -- predicting"))
+# predict_ensemble(rf_mods, dat_iters_arrow_cld) %>%
+#   qsave("wp2-geobia/dat/processed/prediction/random_forest/random_forest_prediction.qs", nthreads = ncores)
 
--- dev/06-susceptibility_modelling/05_create_rasters NOE.R
+++ dev/06-susceptibility_modelling/05_create_rasters NOE.R
@@ -25,24 +25,22 @@ fl_preds <- list.files(glue("wp2-geobia/dat/processed/prediction/{mod_type}"), f
 res <- lapply(fl_preds, qread, nthreads = ncores) %>%
   bind_rows()
 print(glue::glue("{Sys.time()} -- saving full prediction dataframe"))
-qsave(res, glue("dat/processed/prediction/{mod_type}_prediction_mean_sd_sf.qs"), nthreads = 16L)
+qsave(res, glue("wp2-geobia/dat/processed/prediction/{mod_type}_prediction_mean_sd_sf.qs"), nthreads = ncores)
 
-# res <- qread(glue("dat/processed/prediction/{mod_type}_prediction_mean_sd_sf.qs"), nthreads = 16L)
-# > object.size(res) %>% format("auto")
-# [1] "24 Gb"
-
-res <- qread(glue("wp2-geobia/dat/processed/prediction/{mod_type}/{mod_type}_prediction.qs"), nthreads = ncores)
+res <- qread(glue("wp2-geobia/dat/processed/prediction/{mod_type}_prediction_mean_sd_sf.qs"), nthreads = ncores)
 object.size(res) %>% format("auto")
-# [1] "19.5 Mb"
+# [1] "24 Gb" carinthian file
+# [1] "12.4 Gb" NOE file
 
 print(glue::glue("{Sys.time()} -- rasterizing mean"))
 res %>%
-  select(mean_susceptibility = mean_susc, geometry) %>%
+  dplyr::select(mean_susceptibility = mean_susc, geometry) %>%
   st_rasterize() %>%
   write_stars(glue("wp2-geobia/dat/reporting/susceptibility_mean_{mod_type}.tif"), type = "Float32", NA_value = -1)
 # cmd1 <- glue("gdalwarp -cutline wp2-geobia/dat/interim/aoi/NOE_gaiaArea_3416.shp -crop_to_cutline -overwrite wp2-geobia/dat/reporting/susceptibility_mean_{mod_type}.tif wp2-geobia/dat/reporting/susceptibility_mean_{mod_type}_cut.tif")
 # system(command = cmd1)
 
+## TO DO: check if it's cut or it needs to be cut to the area
 
 # # cutting (not needed, it's already cropped to the area)
 # raster <- raster(glue("wp2-geobia/dat/reporting/susceptibility_mean_{mod_type}.tif"))
@@ -58,5 +56,7 @@ res %>%
   select(sd_susceptibility = sd_susc, geometry) %>%
   st_rasterize() %>%
   write_stars(glue("wp2-geobia/dat/reporting/susceptibility_sd_{mod_type}.tif"), type = "Float32", NA_value = -1)
-cmd2 <- glue("gdalwarp -cutline dat/raw/aoi/gaia_aoi_ktn_3416.gpkg -crop_to_cutline -overwrite dat/reporting/susceptibility_sd_{mod_type}.tif dat/reporting/susceptibility_sd_{mod_type}_cut.tif")
-system(command = cmd2)
+# cmd2 <- glue("gdalwarp -cutline dat/raw/aoi/gaia_aoi_ktn_3416.gpkg -crop_to_cutline -overwrite dat/reporting/susceptibility_sd_{mod_type}.tif dat/reporting/susceptibility_sd_{mod_type}_cut.tif")
+# system(command = cmd2)
+
+## TO DO: check if it's cut or it needs to be cut to the area
